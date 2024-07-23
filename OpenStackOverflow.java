774b8e806c761e2593a8b46febba987c479936a0
Theri Muthu Selvam <theriselvam37@gmail.com>
2024-07-23 13:26:13 +0530

    added logs

--- OpenStackOverflow.java
+++ OpenStackOverflow.java
@@ -4,10 +4,13 @@ import java.net.URI;
 public class OpenStackOverflow {
     public static void main(String[] args) {
         try {
+            System.out.println("StackOverflow Entry");
             URI url = new URI("https://stackoverflow.com");
             Desktop.getDesktop().browse(url);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("StackOverflow Exit");
         }
     }
 }
